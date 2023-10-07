f5b7ec5cccddcec94e45623f3bc31e28a936f3fe
Gerald Akinola <akinolagerald@gmail.com>
2023-10-07 08:47:59 -0700

    herosection

--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -9,8 +9,11 @@ function Herosection () {
                 <h2 className="title">RECEIVE MONEY FASTER WITH CAMPUSPAY</h2>
                 <p className="other">Securely receive Business Payments, Association Dues, Donations, Tickets Fees and Contributions anywhere on campus.</p>
                  <div className="banner-img"></div>
+                  <div className="banner-buttons">
+                <button>Get Started</button>
+                <button>Contact Sales</button>
+            </div> 
             </div>
-           
            </div>
         </herosection>
     )
